150be8f5a3d8da342e9a2952dc675bdd83de7bdd
Quramy <yosuke.kurami@gmail.com>
2024-10-03 05:01:12 +0900

    chore: Move hook to support dir

--- src/app/routes/posts.$postId/route.tsx
+++ src/app/routes/posts.$postId/route.tsx
@@ -2,13 +2,11 @@ import { Suspense } from "react";
 import type { LoaderFunctionArgs } from "@remix-run/node";
 import { createQueryPreloader, QueryRef } from "@apollo/client/index.js";
 
-import { InternalQueryReference } from "@apollo/client/react/internal/cache/QueryReference.js";
+import { useReadQuery } from "#support/apollo";
+import { useLoaderData } from "#support/remix";
 
 import { graphql, type DocumentType } from "#app/gql";
 import { getSingletonApolloClient } from "#app/lib/apolloClient";
-
-import { useReadQuery } from "#app/hooks/useReadQuery";
-import { useLoaderData } from "#support/remix";
 
 const query = graphql(`
   query PostDetail_Query($postId: ID!) {
